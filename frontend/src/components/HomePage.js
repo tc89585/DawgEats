43b34399c0d56171353862b0188782d3c2d83733
tc89585 <90994770+tc89585@users.noreply.github.com>
2023-04-16 16:11:27 -0400

    Update HomePage.js
    use new and improved RestaurantCard

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import NavBar from './NavBar.js';
 import Restaurant from './Restaurant.js';
+import RestaurantCard from './RestaurantCard.js';
 import { useState } from 'react';
 
 export default function HomePage() {
-  const [restaurants, setRestaurants] = useState([]);
+  /*dummy list for testing purposes*/
+  const [restaurants, setRestaurants] = useState([
+    {
+      id: 1,
+      restName: 'Taichi Bubble Tea',
+      address: '247 E Broad St, Athens, GA 30601',
+      contact: '706-395-6483',
+      cuisine: 'asian',
+      image:
+        'https://lh3.googleusercontent.com/p/AF1QipNrqoDjdkfcLJAIHgqim9ATpX5f0v8zgMVRCRia=s1360-w1360-h1020',
+      description: 'A Bubble tea spot',
+    },
+    {
+      id: 2,
+      restName: 'sdfgdsgs',
+      address: '247 E Broad St, Athens, GA 30601',
+      contact: '706-395-6483',
+      cuisine: 'asian',
+      image:
+        'https://lh3.googleusercontent.com/p/AF1QipNrqoDjdkfcLJAIHgqim9ATpX5f0v8zgMVRCRia=s1360-w1360-h1020',
+      description: 'A Bubble tea spot',
+    },
+    {
+      id: 3,
+      restName: 'agsaasgad',
+      address: '247 E Broad St, Athens, GA 30601',
+      contact: '706-395-6483',
+      cuisine: 'asian',
+      image:
+        'https://lh3.googleusercontent.com/p/AF1QipNrqoDjdkfcLJAIHgqim9ATpX5f0v8zgMVRCRia=s1360-w1360-h1020',
+      description: 'A Bubble tea spot',
+    },
+  ]);
 
-  /*call back function to delete a Card...
-    button will be clicked, this will be called.
-  */
   const handleRemoveCard = (id) => {
     let updatedList = restaurants.filter(
       (restaurant) => !(restaurant.id === id)
@@ -19,27 +49,22 @@ export default function HomePage() {
   return (
     <div className="home-page">
       <NavBar />
-      <img
-        src="./plus-icon.svg"
-        alt="plus icon"
-        id="plus-icon"
-        style={{ width: 30, height: 30, cursor: 'pointer' }}
-      />
+      <a href="/create-item">
+        <img
+          src="./plus-icon.svg"
+          alt="plus icon"
+          id="plus-icon"
+          style={{ width: 30, height: 30, cursor: 'pointer' }}
+        />
+      </a>
       <span>Add New</span>
       <div className="grid">
-        {restaurants.map((restaurant) => {
-          return (
-            <Restaurant
-              key={restaurant.id}
-              name={restaurant.name}
-              image={restaurant.image}
-              description={restaurant.description}
-              onClick={() => handleRemoveCard(restaurant.id)}
-            />
-          );
+        {restaurants.map((restaurant, index) => {
+          return <RestaurantCard item={restaurants[index]} />;
         })}
       </div>
     </div>
   );
 }
+
 
