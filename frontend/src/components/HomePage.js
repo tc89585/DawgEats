9c2172e7cbf4fb2923d522651e74fb5a47be5b9f
tc89585 <90994770+tc89585@users.noreply.github.com>
2023-04-16 14:52:22 -0400

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,6 +4,50 @@ import Restaurant from './Restaurant.js';
 import { useState } from 'react';
 
 export default function HomePage() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  /*call back function to delete a Card...
+    button will be clicked, this will be called.
+  */
+  const handleRemoveCard = (id) => {
+    let updatedList = restaurants.filter(
+      (restaurant) => !(restaurant.id === id)
+    );
+    setRestaurants(updatedList);
+  };
+
+  return (
+    <div className="home-page">
+      <NavBar />
+      <img
+        src="./plus-icon.svg"
+        alt="plus icon"
+        id="plus-icon"
+        style={{ width: 30, height: 30, cursor: 'pointer' }}
+      />
+      <span>Add New</span>
+      <div className="grid">
+        {restaurants.map((restaurant) => {
+          return (
+            <Restaurant
+              key={restaurant.id}
+              name={restaurant.name}
+              image={restaurant.image}
+              description={restaurant.description}
+              onClick={() => handleRemoveCard(restaurant.id)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+import React from 'react';
+import NavBar from './NavBar.js';
+import Restaurant from './Restaurant.js';
+import { useState } from 'react';
+
+export default function HomePage() {
   /*dummy list for testing purposes*/
   const [restaurants, setRestaurants] = useState([
     {
