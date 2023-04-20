d4a2e664f9996717adf105dd5c33b838aeca1edf
tc89585 <tc89585@uga.edu>
2023-04-20 14:43:20 -0400

    fix login form css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,33 +2,50 @@ import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
-    return (
-    <form>
-        <h1>DawgsEat Login</h1>
+  return (
+    <div className="login">
+      <form>
+        <h1>Dawg Eats Login</h1>
         <br></br>
-        <label for="username">Username:</label><br/>
-        <input type="text" id="username" name="username" placeholder="username"/><br/>
-        <label for="username">Password:</label><br/>
-        <input type="password" id="password-field" class="login-form-field" placeholder="Password"></input>
-            <br></br>
-	    <Link to= '/'>
-            <input className="button" type="submit" value="Login" id="login-form-submit"></input>
-	    </Link>
+        <label for="username">Username:</label>
+        <br />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+        />
+        <br />
+        <label for="username">Password:</label>
+        <br />
+        <input
+          type="password"
+          id="password-field"
+          class="login-form-field"
+          placeholder="Password"
+        ></input>
+        <br></br>
+        <Link to="/">
+          <input
+            className="button"
+            type="submit"
+            value="Login"
+            id="login-form-submit"
+          ></input>
+        </Link>
 
-	<br></br>
-    <br></br>
+        <br></br>
+        <br></br>
 
-	    <Link to= '/create-user'>
-	    <input type="submit" className="button" value="Create Account"></input>
-	</Link>
-
-    </form>
-    
-    
-    
-    
-    )
-    
-    
-  }
-  export default Login;
+        <Link to="/create-user">
+          <input
+            type="submit"
+            className="button"
+            value="Create Account"
+          ></input>
+        </Link>
+      </form>
+    </div>
+  );
+}
+export default Login;
