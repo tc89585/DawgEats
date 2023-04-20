30f544fab9e4d2f332206869ae67d861f610b3d8
tc89585 <tc89585@uga.edu>
2023-04-19 20:33:07 -0400

    added .env to protect database password

--- backend/app.js
+++ backend/app.js
@@ -3,28 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 3001;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const restaurants = require('./routes/api/restaurants');
 
 app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json({ extended: false }));
-
-app.get('/', (request, response) => {
-  response.send('hey');
-});
-
 app.use('/api/restaurants', restaurants);
 
-/* 
-    app.get('/', (request, response) => {
-    response.send('hey');
-    });
-*/
-
 /*Connect to database*/
-const CONNECTION_STRING =
-  'mongodb+srv://groupEweb:CprddcWV5VDYpakX@dawgeats.o982vrf.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_STRING = `mongodb+srv://groupEweb:${process.env.DB_PASSWORD}@dawgeats.o982vrf.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set('strictQuery', false);
 mongoose
   .connect(CONNECTION_STRING, {
