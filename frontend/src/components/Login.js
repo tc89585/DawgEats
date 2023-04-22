a6811105b53ba76e95b61eba84ec6af80eda752c
arnabbiswas0 <043.biswasarnab@gmail.com>
2023-04-21 22:45:56 -0400

    update login.js
    Added handler for login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,23 @@
+import { useContext, useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+
+
+const [email, setEmail] = useState();
+const [password, setPassword] = useState();
+const [error, setError] = useState();
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate();
+const { setUserData} = useContext(UserContext);
 
 function Login() {
     return (
     <form>
         <h1>DawgsEat Login</h1>
         <br></br>
-        <label for="username">Username:</label><br/>
+        <label for="email">Email:</label><br/>
         <input type="text" id="username" name="username" placeholder="username"/><br/>
         <label for="username">Password:</label><br/>
         <input type="password" id="password-field" class="login-form-field" placeholder="Password"></input>
@@ -31,4 +42,24 @@ function Login() {
     
     
   }
-  export default Login;
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    try {
+        const loginUser = {email, password};
+        const loginRes = await axios.post("http://localhost:3000/api/users/login", loginUser);
+        setUserData({
+            token: loginRes.data.token,
+            user: loginRes.data.user,
+        });
+        localStorage.setItem.setItem("auth.token", loginRes.data.token);
+        navigate('/');
+
+
+    } catch (err) {
+        setLoading(false);
+        error.response.data.msg && setError(err.response.data.msg);
+    }
+}
+export default Login;
