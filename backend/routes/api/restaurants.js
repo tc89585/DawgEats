d1725b17fc92304f789fd5309a55ac5164209c3d
Spc69655 <95435773+Spc69655@users.noreply.github.com>
2023-04-24 04:27:30 -0400

    edited routes

--- backend/routes/api/restaurants.js
+++ backend/routes/api/restaurants.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { ObjectId } = require('mongodb');
 
-const Restaurant = require('../../models/user');
+const {Restaurant} = require('../../models/restaurant');
 const User = require('../../models/user');
 
 router.get('/', async (request, response) => {
@@ -21,23 +22,52 @@ router.get('/restaurant/:id', (req, res) => {
 	.catch((err) => res.status(404).json({noitemfound: 'No restaurant found'}));
 });
 
-router.post('/create-restaurant', (req, res) => {
-    Restaurant.create(req.body)
-	.then((item) => res.json({msg: 'added sucessfully'}))
-	.catch((err) => res.status(404).json({error: 'unable to add'}));
-});
+router.post('/create-restaurant/:userId', async (req, res) => {
+    const restaurant =  await Restaurant.create(req.body);
+	
+		User.findByIdAndUpdate({_id: req.params.userId}, {$push: {restaurants : restaurant}})
+		.then((item) => res.json({msg: 'added sucessfully'}))
+		.catch((err) => res.status(404).json({error: 'unable to add'}))
+	});
 
-router.put('/edit-restaurant/:id', (req, res) => {
-    Restaurant.findByIdAndUpdate(req.params.id, req.body)
-	.then((item) => res.json({msg: 'updated sucessfully'}))
-	.catch((err) => res.status(404).json({error: 'unable to update'}));
-});
+	router.put('/edit-restaurant/:id/:userId', async (req, res) => {
+		const {
+			id,
+			userId
+		  } = req.params
 
-router.delete('/delete-restaurant/:id',  (req, res) => {
-    Restaurant.findByIdAndRemove(req.params.id, req.body)
-	.then((item) => res.json({msg: 'deleted sucessfully'}))
-	.catch((err) => res.status(404).json({error: 'unable to delete'}));
-});
+		  
+
+		await Restaurant.findByIdAndUpdate(id, req.body);
+
+	 const user = await User.findOneAndUpdate( {"_id" :userId, "restaurants._id": id }, {$set:  {
+           "restaurants.$.name": req.body.name,
+		   "restaurants.$.address": req.body.address,
+		   "restaurants.$.number": req.body.number,
+		   "restaurants.$.cuisine": req.body.cuisine,
+		   "restaurants.$.description": req.body.description,
+		   "restaurants.$.imageUrl": req.body.imageUrl,
+        }}, {
+			new : false,
+			overwrite : true,
+			runValidators: true
+		  })
+		.then((item) => res.json({msg: 'updated sucessfully'}))
+		.catch((err) => res.status(404).json({error: 'unable to update'}));
+	});
+
+	router.delete('/delete-restaurant/:id/:userId',  async (req, res) => {
+
+		const {
+			id,
+			userId
+		  } = req.params
+
+		await Restaurant.findByIdAndRemove(id, req.body);
+		 User.findByIdAndUpdate(userId, {'$pull': { 'restaurants':{'_id':  new ObjectId(id)} }})
+		.then((item) => res.json({msg: 'deleted sucessfully'}))
+		.catch((err) => res.status(404).json({error: 'unable to delete'}));
+	});
 
 
-module.exports = router;
+	module.exports = router;
